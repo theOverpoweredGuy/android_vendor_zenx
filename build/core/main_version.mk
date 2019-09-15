b272cbf4db263b46e5c293cb37b7efe439c94676
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-15 18:54:45 +0300

    build: set build fingerprint for all devices
    * there's no per device way to add props to /system/build.prop
    * adding it here is the only way so far

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,3 +1,9 @@
+# Build fingerprint
+ifneq ($(BUILD_FINGERPRINT),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+endif
+
 # ZenX-OS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.zenx.build.date=$(ZENX_BUILD_DATE) \
