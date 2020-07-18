fcd6dffef9bf5eee45c871a2f6da52090cd6005f
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-07-18 08:51:40 +0200

    vendor: Fit updater script for our OTA

--- tools/generate_ota_info.sh
+++ tools/generate_ota_info.sh
@@ -4,12 +4,7 @@ then
   file_path=$1
   filename=$(basename "$file_path")
   if [ -f $file_path ]; then
-    version=$(grep ro\.zenx\.base\.version $OUT/system/build.prop | cut -d= -f2);
-    romtype=$(echo $ZENX_BUILD_TYPE);
-    size=$(stat -c%s $file_path);
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
-    id=$(sha256sum $file_path | awk '{ print $1 }');
-    url="https://sourceforge.net/projects/zenx-os/files/$ZENX_BUILD/$filename/download";
-    echo "{\n  \"response\": [\n    {\n      \"filename\": \"$filename\",\n      \"version\": \"v$version\",\n      \"romtype\": \"$romtype\",\n      \"size\": \"$size\",\n      \"datetime\": $datetime,\n      \"id\": \"$id\",\n      \"url\": \"$url\"\n    }\n  ]\n}" > $OUT/$ZENX_BUILD.json
+    echo "{\n  \"response\": [\n    {\n      \"datetime\": $datetime, \n      \"filename\": \"$filename\" \n   }\n  ]\n}" > $OUT/$ZENX_BUILD.json
   fi
 fi
