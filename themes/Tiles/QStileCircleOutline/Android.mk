Genkzsz11/android_vendor_themes

QSTiles: Add CircleOutline styles [2/3]

overlays/QStileCircleOutline/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := QStilesCircleOutline
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)

theOverpoweredGuy/android_vendor_zenx

themes/Tiles/QStileCircleOutline/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := QStileCircleOutline
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)