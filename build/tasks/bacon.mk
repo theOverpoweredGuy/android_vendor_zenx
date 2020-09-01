a20ac2e6493e7f94ce6a7c194bc64ab011707ab0
Raaj52 <mail2raaj52@gmail.com>
2020-09-01 18:58:06 +0000

    build: phony: Enhancement
    - added ZenX-OS ASCII art
    - added "zenx" under  phony to support "make zenx"
    
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,11 +16,32 @@
 # -----------------------------------------------------------------
 # ZenX-OS OTA update package
 
+ifneq ($(BUILD_WITH_COLORS),0)
+    include $(TOP_DIR)vendor/zenx/build/core/colors.mk
+endif
+
 ZENX_TARGET_PACKAGE := $(PRODUCT_OUT)/$(ZENX_BUILD_VERSION).zip
 
-.PHONY: bacon
-bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
+.PHONY: otapackage zenx bacon
+otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
+zenx: otapackage
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(ZENX_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(ZENX_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(ZENX_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/zenx/tools/generate_ota_info.sh $(ZENX_TARGET_PACKAGE)
-	@echo "Package Complete: $(ZENX_TARGET_PACKAGE)" >&2
+	@echo -e ""
+	@echo -e ""
+	@echo -e ""
+	@echo -e ${CL_BLU}"███████╗███████╗███╗   ██╗██╗  ██╗      ██████╗ ███████╗"
+	@echo -e ${CL_BLU}"╚══███╔╝██╔════╝████╗  ██║╚██╗██╔╝     ██╔═══██╗██╔════╝"
+	@echo -e ${CL_BLU}"  ███╔╝ █████╗  ██╔██╗ ██║ ╚███╔╝█████╗██║   ██║███████╗"
+	@echo -e ${CL_BLU}" ███╔╝  ██╔══╝  ██║╚██╗██║ ██╔██╗╚════╝██║   ██║╚════██║"
+	@echo -e ${CL_BLU}"███████╗███████╗██║ ╚████║██╔╝ ██╗     ╚██████╔╝███████║"
+	@echo -e ${CL_BLU}"╚══════╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝      ╚═════╝ ╚══════╝"
+	@echo -e ""
+	@echo -e ${CL_MAG}"======================================================================================================="
+	@echo -e ${CL_RST}"Package: "$(ZENX_TARGET_PACKAGE)
+	@echo -e ${CL_RST}"md5: `cat $(ZENX_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`"
+	@echo -e ${CL_RST}"size:`ls -lah $(ZENX_TARGET_PACKAGE) | cut -d ' ' -f 5`"
+	@echo -e ${CL_MAG}"======================================================================================================="
+
+bacon: zenx
